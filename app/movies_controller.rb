fd427c1081bf5e1375a37b5044b9a26963d14667
Jenny K <jkam17@gmail.com>
2018-09-21 19:43:44 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -39,11 +39,11 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.find(-1)
+  Movie.find(last_insert_rowid())
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.all.length
 end
 
 def can_find_the_first_item_from_the_database_using_id
